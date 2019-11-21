592a48f92fb69d01c63f146cd9f3c2a24fc5bb07
Piper <piperwei97@gmail.com>
2019-11-22 11:06:45 +1300

    first commit

---
+++ MarsFramework/Test/Program.cs
@@ -0,0 +1,35 @@
+﻿using MarsFramework.Global;
+using MarsFramework.Pages;
+using NUnit.Framework;
+
+
+
+namespace MarsFramework
+{
+    [TestFixture(BrowserType.Firefox)]
+    [TestFixture(BrowserType.Chrome)]
+    [Parallelizable(ParallelScope.Fixtures)]
+    [Category("Sprint1")]
+    public class Program : Base
+
+    {
+        public Program(BrowserType browser) : base(browser)
+        {
+        }
+        [Test]
+        public void CreatNewSkill()
+        {
+
+            //Create Extent Report
+            test = extent.StartTest("Share Skills");
+            // Create Share Skills      
+            ShareSkills obj = new ShareSkills(_driver);
+            obj.AddNewSkill();
+
+        }
+    
+
+    }
+}
+
+
